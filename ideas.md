c47166a09e8dffb1e1ecf37829522cda93d51c57
Theogen Ratkin <feogenratkin@gmail.com>
2022-04-03 16:34:13 -0400

    First commit

---
+++ ideas.md
@@ -0,0 +1,12 @@
+# Mini Jam 103
+
+Limitation: You are your own enemy
+Optional Theme: Darkness
+
+## Ideas
+
+### Building factories and causing pollution
+
+### Collecting lights and having to find your way back in the darkness
+
+
---
+++ script/player.gd
@@ -0,0 +1,91 @@
+extends KinematicBody2D
+
+signal on_jump
+
+var can_move = true
+
+var snap = false
+
+var run_speed = 50
+var jump_speed = -140
+var gravity = 400
+
+var velocity = Vector2()
+
+var startx
+
+onready var sprite = get_node("sprite")
+var eyelids
+
+var gender
+
+var idle_name = "idle"
+
+const JUMP_BUFFER = 0.3
+var _jump_buffer = 0.0
+
+func _ready():
+	#eyelids = get_node("eyelids")
+	#blink(true)
+	align_position_to_pixels()
+	startx = position.x
+	#set_active(false)
+
+
+func set_active(state):
+	can_move = state
+	visible = state
+
+
+func align_position_to_pixels():
+	position = Vector2(round(position.x), position.y)
+
+
+func flip(state):
+	transform.x = Vector2(-1 if state else 1, 0)
+
+
+func get_input():
+	if not can_move:
+		velocity.x = 0
+		sprite.play(idle_name)
+		align_position_to_pixels()
+		return
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('jump')
+	
+	if not is_on_floor() and jump:
+		_jump_buffer = JUMP_BUFFER
+	
+	if is_on_floor() and (jump or _jump_buffer > 0):
+		velocity.y = jump_speed
+		_jump_buffer = 0
+	
+	if right:
+		flip(false)
+		velocity.x += run_speed
+	if left:
+		flip(true)
+		velocity.x -= run_speed
+	if !right and !left:
+		if is_on_floor():
+			sprite.play("idle")
+		flip(false)
+		align_position_to_pixels()
+	
+	if !is_on_floor():
+		if !right and !left:
+			sprite.play("jump")
+
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	if not snap:
+		velocity = move_and_slide(velocity, Vector2(0, -1))
+	else:
+		velocity = move_and_slide_with_snap(velocity, Vector2(0, 8), Vector2(0, -1))
+	if _jump_buffer > 0:
+		_jump_buffer -= delta
